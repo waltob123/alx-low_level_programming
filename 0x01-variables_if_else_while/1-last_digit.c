637a4f40a9841edfff14617fb81ad5e0c1999b30
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-17 14:13:55 +0000

    Moved variable declaration to the top

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,11 +16,11 @@ int main(void)
 {
 	int n;
 
+	int last_digit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int last_digit;
-
 	last_digit = n % 10;
 	if (last_digit > 5)
 	{
