8c5a1a870cc2343e7ceb2fd27d6bd9f595f0a670
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-17 07:32:09 -0800

    Added file 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+	printf("\n");
+}
