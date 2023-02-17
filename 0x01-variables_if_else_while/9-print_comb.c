93668dc2fa9cf0724c929b61ad9c4fbfd2e73b57
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-17 08:29:49 -0800

    Added file 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(j + '0');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
