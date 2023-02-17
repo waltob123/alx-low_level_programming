d11509531aef361f2cca2c60dba8675e3eb6ee03
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-17 14:04:15 +0000

    Added source file 1-last_digit.c
    I added lines of code to get the last digit of a random and print out a statement based
    on some conditions

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * prints the last digit of n
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int last_digit;
+	last_digit = n % 10;
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, last_digit);
+	}
+	if (last_digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, last_digit);
+	}
+	if (last_digit < 6 && last_digit > -1)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0);
+	}
+	return (0);
+}
