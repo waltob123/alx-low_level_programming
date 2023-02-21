53155539435bf516eeb665c4945c96caf2c8ce34
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-21 12:53:49 +0000

    Added C source file 0-putchar
    Inside this file is a function that prints _putchar.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * prints _putchar and ends with a new line
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
