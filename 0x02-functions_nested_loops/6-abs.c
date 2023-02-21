c8e7b1398fdf76d2e2ee1e2e75645ddd102ebdaa
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-21 10:23:07 -0800

    Added file 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute form of a number
+ *
+ * @n: number to convert
+ *
+ * Return: 0 (Success)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = 0 - n;
+	return (0);
+}
