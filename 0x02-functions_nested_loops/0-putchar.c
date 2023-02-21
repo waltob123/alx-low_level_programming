5b0b12e87ab3877648a89a302c5309671e9ca606
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-21 09:15:12 -0800

    Added file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * print _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
