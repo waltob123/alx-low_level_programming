95e8e7f4f8a8f4aa85e7784ddac8324436773142
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-21 13:31:27 +0000

    Added C source file 1-alphabet
    Added a function prototype to main.h to print alphabets in lowercase and called it
    in the 1-alphabet.c main function.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,12 @@ int _putchar()
 {
 	return (0);
 }
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+}
