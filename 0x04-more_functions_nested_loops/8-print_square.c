05520b217fb2092206261da34805aa23cb5e34b4
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-23 22:10:19 +0000

    Added file 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print squares using #
+ * @size: number of squares to print
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		while (i < n)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			i++;
+			_putchar('\n');
+		}
+	}
+}
