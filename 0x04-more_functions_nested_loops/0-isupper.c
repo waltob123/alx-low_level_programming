3ad524fe65bd27ff21969e83bce4978f0c60e05f
Kwasi Asamoah <fsociety123@Nanas-MacBook-Pro-2.local>
2023-02-23 10:48:56 +0000

    Adding first solution file and header file
    Added files 0-isupper.c and main.h
    Inside 0-isupper is a function that checks if the argument passed to it is an
    uppercase letter and returns 1 if True and 0 if false. In the main.h file
    contains all function prototypes.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - This function checks if the argument passed is an uppercase letter
+ *
+ * @c: The argument passed to function
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+    if ((c >= 65) && (c < 91))
+        return (1);
+    else
+        return (0);
+}
