0e5e1b3d259f373f260f1c5166402f6c8dfac756
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-23 23:18:15 +0000

    Altered file 3-print_remaining_days.c in 0x03

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,10 +2,9 @@
 #include "main.h"
 
 /**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
+* print_remaining_days - takes a date and prints how many days are left
+* @month: month
+* @day: day
 * @year: year
 * Return: void
 */
