466d87e8a60144801a98d7bbd228dbcb24371a99
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-23 14:25:27 +0000

    Added file 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal lines using \
+ * @n: number of times to print
+ */
+
+
+void print_diagonal(int n)
+{
+	int i;
+
+	i = 1;
+
+	while (i <= n)
+	{
+
+		for (; i < n; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		i++;
+	}
+	_putchar('\n');
+}
