293ccb43ca2487fe4a32f1a9cff50c9ce4f0cf64
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-27 08:19:31 -0800

    Declared str_len at the top in 4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@
 
 void print_rev(char *s)
 {
-	int count, i;
+	int count, i str_len;
 	count = i = 0;
 
 	while (s[count] != '\0')
 		count++;
 
-	int str_len = count - 1;
+	str_len = count - 1;
 
 	for (; str_len >= i; str_len--)
 		_putchar(s[str_len]);
