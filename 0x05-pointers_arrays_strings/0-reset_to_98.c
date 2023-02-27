77dd0f0762ea984cb8c0fb3adb0f89789237e1e5
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-27 13:59:28 +0000

    Initial commit
    Created the 0x05-pointers_arrays_strings directory and added a README
    file to it and also add a main.h and the solution source file to the
    first problem.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Resets the pointer value to 98
+ * @n: pointer variable to reset
+ * Return: void
+*/
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains all solutions to ALX-SE pointers, arrays and strings part 1 projects.
