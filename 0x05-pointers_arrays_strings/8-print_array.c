f0874aa21979493f39a25137ed9ddee297f66f70
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-27 15:17:09 -0800

    Fixed bug in code 8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,6 +13,11 @@ void print_array(int *a, int n)
 	int i = 0;
 
 	for (; i < n; i++)
-		printf("%d, ", a[i]);
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
 	_putchar('\n');
 }
