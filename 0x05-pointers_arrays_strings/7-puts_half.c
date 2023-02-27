93148a2dc843c2b43583677ed4b677ff46f1b4c0
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-27 11:05:06 -0800

    Renamed 6-puts to 6-puts2 and added file 7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string.
+ * @str: pointer to string.
+ */
+void puts_half(char *str)
+{
+	int i, len, j;
+
+	i = len = 0;
+
+	while (str[len] != '\n')
+		len++;
+
+	if ((len % 2) == 0)
+		j = len / 2;
+
+	else
+		j = (len + 1) / 2;
+
+	for (i = j; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
