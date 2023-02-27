c52950e9ce44e9845d3dce8a0fd1f2242797e064
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-02-27 08:15:47 -0800

    Added file 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints the reverse of a string
+ * @s: pointer to the string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int count = i = 0;
+
+	while (s[count] != '\0')
+		count++;
+
+	int str_len = count - 1;
+
+	for (; str_len >= i; str_len--)
+		_putchar(s[str_len]);
+
+	_putchar('\n');
+}
