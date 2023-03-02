ff18b2cff4836d653fbc80d98dff36194137709a
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-03-02 05:07:42 -0800

    Fixed error in file 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,16 +10,18 @@ char *leet(char *a)
 {
 	char *lower = "aeotl";
 	char *upper = "AEOTL";
-	char replacers[] = {4, 3, 0, 7, 1};
+	int replacers[] = {'4', '3', '0', '7', '1'};
+	int i;
+	char *a_ptr = a;
 
 	while (*a != '\0')
 	{
-		for (; i < len; i++)
+		for (i = 0; i < 5; i++)
 		{
 			if (*a == *(lower + i) || *a == *(upper + i))
-				*a = replacers[i]
+				*a = replacers[i];
 		}
 		a++;
 	}
-	return (a);
+	return (a_ptr);
 }
