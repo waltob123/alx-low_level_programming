4d3e35f5be6c03b0a14c827cbbc3aa5c3be10e86
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-03-07 00:15:43 +0000

    t
    Added file 9-set_string.c

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer
+ * @to: pointer to assign
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
