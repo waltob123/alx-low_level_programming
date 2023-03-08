9aad4dca803a275a91bc992fb2d2f8f02e36dda4
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-03-08 23:14:49 +0000

    Fixed error in 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,9 +9,9 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == 0 && s2 == 0)
 		return (1);
-	else if (s1 == NULL || s2 == NULL)
+	else if (s1 == 0 || s2 == 0)
 		return (0);
 
 	if (*s1 == '\0')
