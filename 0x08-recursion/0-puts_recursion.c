ec135c8d3424b2e9f8efacd33fa3ba1f2411c8ce
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-03-08 11:45:15 +0000

    Fixed error in 0-puts_recursive.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,8 +11,7 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		s++;
-		_puts_recursion(s);
+		_puts_recursion(s++);
 	}
 	if (*s == '\0')
 		_putchar('\n');
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 int _putchar(char);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 
 #endif
