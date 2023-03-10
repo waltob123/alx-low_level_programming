32b3cc01e4245cc68c943631a22eec340fea531c
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-03-10 12:04:23 +0000

    Initial commit

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if an chara is upper
+ * @c: chara to check
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,2 @@
+This directory contains all solution files for the ALX-SE low-level programming static
+libraries.
