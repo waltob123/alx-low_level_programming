525d2590eea6997adc71f7b5696ad099649eb050
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-03-14 08:06:59 -0700

    Changed ch to pointer in 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char ch = malloc(sizeof(c) * size);
+	char *ch = malloc(sizeof(c) * size);
 
 	if (size == 0)
 		return (NULL);
