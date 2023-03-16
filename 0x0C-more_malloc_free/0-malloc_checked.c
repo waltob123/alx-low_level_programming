9f2ea043566064382a9ea20f1f5d2e2c6fd02a55
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-03-16 13:35:10 +0000

    Initial commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size
+ * Return: pointer to allocated space or 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains all solution files for the alx-se low-level programming more-malloc project.
