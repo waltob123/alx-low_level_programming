8065c473eceb810d3d585678731262f3c4e47cc0
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-03-22 10:46:02 -0700

    Added file 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ * @name: name to print
+ * @f: pointer to function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
