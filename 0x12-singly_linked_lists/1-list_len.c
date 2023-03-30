ec86be8e0e0cc76e44f1869feb943fb12d5e3584
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-03-30 22:17:35 +0000

    Fixed bug in 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,13 +8,14 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	const list_t *temp;
+	unsigned int counter = 0;
 
-	while (h)
+	temp = h;
+	while (temp)
 	{
-		elements++;
-		h = h->next;
+		counter++;
+		temp = temp->next;
 	}
-
-	return (elements);
+	return (counter);
 }
