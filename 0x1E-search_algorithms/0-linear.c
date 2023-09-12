d93655169d87164cab4825d678c9f94484490491
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-09-12 00:07:57 +0000

    Added a new project directory 0x1E-search_algorithms
    Added a README file at the root of the directory and also added the
    solution files of the mandatory tasks of the project search_algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+  * linear_search - Searches for a target value in an array
+  *                 of integers using linear search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The target value to search for.
+  *
+  * Return: -1 if array is NULL or target value was not found.
+  *         first index of target value match.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; index < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,3 @@
+# 0x1E-search_algorithms
+
+This directory contains all solution files for the project 0x1E-search_algorithms
